6f967ae60e9a4d10def9d04997d635954f9171c5
Bernardiho <Bernardarikuoko@gmail.com>
2024-09-23 10:22:55 +0100

    Trying new Fonts

---
+++ assets/js/data/data.js
@@ -0,0 +1,193 @@
+const data = {
+    "bannerSlider": [
+        {
+            "subheader": "Nigeria's Leading Property Management Solution",
+            "header": "Streamline Your Property Management, Rent Collection, and Maintenance with Ease",
+            "img": "./assets/img/property/property1.jpg"
+        },
+        {
+            "subheader": "Transparent, Accountable, and No Hidden Costs",
+            "header": "Take Full Control of Your Real Estate Portfolio",
+            "img": "./assets/img/property/property2.jpeg"
+        }
+    ],
+    "cta": {
+        "cta_text": "Got a Question? Reach Out to Us",
+        "cta_number": "+012 345 6789"
+    },
+    "statistics": {
+        "clients": 130,
+        "properties": 1234,
+        "locations": 45
+    },
+    "aboutSection": {
+        "title": "leading-edge property management platform designed for landlords, real estate companies, and property managers.",
+        "content": "Our goal is to simplify your property management process, from rent collection to maintenance tracking, while providing full transparency and accountability.",
+        "features": [
+            "User-Friendly Interface",
+            "Advanced Reporting Tools",
+            "Automated Maintenance Requests",
+            "24/7 Support",
+            "Secure Payment System",
+            "Real-Time Insights"
+        ],
+        "img": "./assets/img/property/slide/slider-welcome.png"
+    },
+    "featureSection": {
+        "subheader": "Who Can Benefit from Xtate?",
+        "header": "Manage Properties, Tenants, and Agents with Xtate",
+        "content": [
+            {
+                "title": "Landlords",
+                "img": "./assets/img/property/slide/slider-welcome.png",
+                "features": {
+                    "title": "Manage your properties efficiently with powerful tools.",
+                    "list": [
+                        "Complete Portfolio Management",
+                        "Track Rent Collection and Maintenance",
+                        "Automated Reports and Analytics",
+                        "Tenant Communication"
+                    ]
+                }
+            },
+            {
+                "title": "Property Managers",
+                "img": "./assets/img/property/slide/slider-tenants.png",
+                "features": {
+                    "title": "Everything you need to manage properties and tenants.",
+                    "list": [
+                        "Vacancy Tracking",
+                        "Tenant Screening",
+                        "Maintenance Requests",
+                        "Rent Collection"
+                    ]
+                }
+            },
+            {
+                "title": "Tenants",
+                "img": "./assets/img/property/slide/slider-tenants.png",
+                "features": {
+                    "title": "A seamless rental experience for tenants.",
+                    "list": [
+                        "Online Rent Payments",
+                        "Maintenance Requests",
+                        "Lease Management",
+                        "Document Access"
+                    ]
+                }
+            },
+            {
+                "title": "Agents",
+                "img": "./assets/img/property/slide/slider-tenants.png",
+                "features": {
+                    "title": "Supporting agents with efficient workflows.",
+                    "list": [
+                        "Inspection Scheduling",
+                        "Application Processing",
+                        "Real-Time Communication"
+                    ]
+                }
+            }
+        ]
+    },
+    "testimonialSection": [
+        {
+            "name": "David Abraham",
+            "profession": "Software Developer",
+            "testimonial": "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam"
+        },
+        {
+            "name": "Victor Eyoung",
+            "profession": "Lawyer",
+            "testimonial": "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam"
+        },
+        {
+            "name": "Victoria Omolaowu",
+            "profession": "Director",
+            "testimonial": "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam"
+        }
+    ],
+    "resquestQuoteSection": "Looking for more information? Let our team assist you with custom property management solutions tailored to your specific needs. Contact us today for a free consultation and get a quote that meets your business goals.",
+    "faqSection": [
+        {
+            "question": "How does Xtate help landlords manage their properties?",
+            "answer": "Xtate helps landlords oversee rental properties by providing tools for managing tenants, tracking rent collection, and handling maintenance issues. It offers a comprehensive dashboard for property portfolio management, financial overview, tenant screening, and maintenance tracking."
+        },
+        {
+            "question": "What are the benefits of using Xtate for property managers?",
+            "answer": "Property managers can share vacant properties with agents, manage tenant applications, create inspections, and report maintenance issues all in one platform. Xtate also offers tools for rent collection, lease management, and generating comprehensive financial reports."
+        },
+        {
+            "question": "Can tenants use Xtate to pay rent online?",
+            "answer": "Yes, tenants can pay rent, download invoices, sign agreements, and report maintenance issues directly within the Xtate app. The system supports various payment methods including bank transfers and credit cards, with options for setting up automatic payments."
+        },
+        {
+            "question": "How secure is the data stored in the Xtate?",
+            "answer": "We take data security very seriously. Our system uses bank-level encryption for all data transmissions and storage. We regularly perform security audits and updates to ensure your data remains protected. Additionally, we implement role-based access control to ensure users only access information relevant to their roles."
+        },
+        {
+            "question": "Is there a mobile app available for Xtate?",
+            "answer": "Yes, we offer mobile apps for both iOS and Android devices. These apps provide core functionality of the Xtate, optimized for mobile use. You can manage properties, handle tenant requests, and access important information on the go."
+        },
+        {
+            "question": "Can landlords customize their Xtate dashboard?",
+            "answer": "Yes, landlords can fully customize their Xtate dashboard to display relevant information such as rent collection status, vacant properties, maintenance requests, and tenant details. The dashboard widgets can be arranged based on personal preferences to enhance visibility and usability."
+        },
+        {
+            "question": "Does Xtate support automated rent reminders?",
+            "answer": "Yes, Xtate provides automated rent reminders to tenants. Notifications can be sent via email or SMS based on user preferences. Landlords and property managers can also configure custom reminders for overdue payments and upcoming lease expirations."
+        },
+        {
+            "question": "How does Xtate handle maintenance requests?",
+            "answer": "Tenants can submit maintenance requests directly through the Xtate app, which are then routed to the property manager or landlord. The system allows for tracking the status of each request, assigning tasks to maintenance staff, and providing updates to tenants."
+        },
+        {
+            "question": "Can documents like lease agreements be signed digitally in Xtate?",
+            "answer": "Yes, Xtate supports digital signatures for lease agreements, contracts, and other important documents. This feature ensures a secure and efficient way to handle paperwork remotely, reducing the need for physical meetings and expediting the leasing process."
+        },
+        {
+            "question": "Does Xtate offer a tenant screening service?",
+            "answer": "Yes, Xtate provides an integrated tenant screening service that includes credit checks, background checks, and rental history verification. This helps landlords and property managers make informed decisions about potential tenants, minimizing risks and ensuring a reliable tenant base."
+        }, {
+            "question": "What types of reports can Xtate generate?",
+            "answer": "Xtate can generate a variety of reports including financial summaries, rent roll reports, vacancy rates, maintenance costs, and tenant communication logs. These reports help landlords and property managers gain insights into their operations, optimize expenses, and improve overall efficiency."
+        },
+        {
+            "question": "Does Xtate support notifications for tenants and property managers?",
+            "answer": "Yes, Xtate supports a robust notification system for both tenants and property managers. Tenants receive notifications about rent due dates, upcoming inspections, or maintenance updates, while property managers get alerts on new tenant applications, overdue rent payments, and other critical events."
+        }
+    ],
+    "contact": {
+        "address": "123 kurudu, street Abuja",
+        "number": "+2348117974441",
+        "email": "info@pms.bougroup.org",
+        "socials": [
+            {
+                "name": "Twitter",
+                "links": "https://www.twitter.com",
+                "icon": "fab fa-twitter fw-normal"
+            },
+            {
+                "name": "Facebook",
+                "links": "https://www.twitter.com",
+                "icon": "fab fa-twitter fw-normal"
+            },
+            {
+                "name": "YouTube",
+                "links": "https://www.twitter.com",
+                "icon": "fab fa-twitter fw-normal"
+            },
+            {
+                "name": "Instagram",
+                "links": "https://www.twitter.com",
+                "icon": "fab fa-twitter fw-normal"
+            }, {
+                "name": "LinkedIn",
+                "links": "https://www.twitter.com",
+                "icon": "fab fa-twitter fw-normal"
+            }
+        ]
+    }
+};
+
+export { data as default };
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,81 @@
-console.log("hello");
+import { MagicViewModel as MagicViewModel_1, AbstractCustomElement as AbstractCustomElement_1, MagicView as MagicView_1 } from './node_modules/@boudev/magicui/lib/cjs/index.js';
+import data from './data/data.js';
+
+class XtateViewModel extends MagicViewModel_1 {
+    constructor(data) {
+        super(data);
+    }
+}
+class Carousel extends AbstractCustomElement_1 {
+    constructor(id) {
+        super();
+        this.id = id;
+    }
+    // connectedCallback() {
+    //     this.render([
+    //         {
+    //             "subheader": "NO.1 PROPERTY MANAGEMENT COMPANY IN NIGERIA",
+    //             "header": "Manage your properties, tenants and rent collection with ease",
+    //             "img": "./assets/img/property/property1.jpg"
+    //         },
+    //         {
+    //             "subheader": "FULL TRANSPARENCY ACCOUNTABILITY AND NO HIDDEN COST",
+    //             "header": "Take control of your Real Estate Portfolio",
+    //             "img": "./assets/img/property/property2.jpeg"
+    //         }
+    //     ]);
+    // }
+    render(data) {
+        if (data == null) {
+            return;
+        }
+        console.log("rendering carousel...");
+        this.innerHTML = '';
+        let sliderContainer = document.createElement('div');
+        sliderContainer.id = 'header-carousel';
+        sliderContainer.classList.add(...['carousel', 'slide', 'carousel-fade', 'min-vh-100']);
+        let parentSliderContainer = document.createElement('div');
+        parentSliderContainer.classList.add(...['carousel-inner', 'vh-100']);
+        data.forEach((slide, index) => {
+            let slideElement = document.createElement('div');
+            slideElement.classList.add(...['carousel-item', 'vh-100']);
+            if (index === 0) {
+                slideElement.classList.add('active');
+            }
+            slideElement.innerHTML = `
+                <img class="w-100 vh-100" src="${slide.img}" alt="Image">
+                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                    <div class="p-3" style="max-width: 900px;">
+                        <h4 class="text-white text-uppercase mb-3 animated slideInDown">${slide.subheader}</h4>
+                        <h2 class="display-4 text-white mb-md-4 animated zoomIn">${slide.header}</h2>
+                        <a href="https://pms.bougroup.org/account/login.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Create free account / Login</a>
+                        <a href="#service" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Learn More</a>
+                    </div>
+                </div>
+            `;
+            parentSliderContainer.appendChild(slideElement);
+        });
+        sliderContainer.appendChild(parentSliderContainer);
+        // sliderContainer.insertAdjacentHTML('beforeend', btnHTML);
+        this.appendChild(sliderContainer);
+    }
+}
+customElements.define('carousel-slider', Carousel);
+class XtateView extends MagicView_1 {
+    constructor(vm) {
+        super(vm);
+        this.Carousel = new Carousel("xtate-carousel");
+        this.registerElement("bannerSlider", this.Carousel);
+        this.registerElement("cta.cta_text", "cta_text");
+        this.registerElement("statistics.clients", "client");
+        this.registerElement("aboutSection.content", "about_content");
+        // this.registerElement("aboutSection.title", "about_title");
+        // this.registerElement("cta.cta_number", "cta_number");
+        // this.registerElement("cta.cta_number", "cta_number");
+        // this.registerElement("cta.cta_number", "cta_number");
+        // this.registerElement("cta.cta_number", "cta_number");
+    }
+}
+const viewModel = new XtateViewModel(data);
+const view = new XtateView(viewModel);
+view.render();
